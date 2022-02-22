ad50d61bfbb2dddb09f7ebd8eaec28989fc8d3de
vm.vdst <vm.vdst@gmail.com>
2022-02-22 10:58:18 +0100

    first commit

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 15,
+    from: '0xHyru',
+    to: '0xMarty',
+    date: '4 months ago',
+  },
+  {
+    price: 18,
+    from: '0xHyru',
+    to: '0xStoic',
+    date: '5 months ago',
+  },
+  {
+    price: 13,
+    from: '0xHyru',
+    to: '0xOmniscient',
+    date: '6 months ago',
+  },
+  {
+    price: 11,
+    from: '0xHyru',
+    to: '0xErfan',
+    date: '7 months ago',
+  },
+]
